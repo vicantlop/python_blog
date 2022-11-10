d133900284f93f1af6337474add93b89fdc3356c
vicantlop <105176088+vicantlop@users.noreply.github.com>
2022-11-10 11:30:41 -0800

    added css and stars image background

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin 
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls), #in termnal use python manage.py createsuperuser to create admin
@@ -8,3 +9,5 @@ urlpatterns = [
     path("about/", views.about),
     path('', views.home)
 ]
+
+urlpatterns += staticfiles_urlpatterns() #will first check to see if we are in debug mode
