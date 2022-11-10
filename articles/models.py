6b39abf85d90e7381cb17be03c7c7f465fd76741
vicantlop <105176088+vicantlop@users.noreply.github.com>
2022-11-10 08:57:06 -0800

    created models

--- articles/models.py
+++ articles/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-  title: 
+  title = models.CharField(max_length=100)
+  slug = models.SlugField()
+  body = models.TextField()
+  date = models.DateTimeField(auto_now_add=True)
+  #add in thumbnail
+  #add in author
