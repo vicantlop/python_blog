0ac3b14174d10251c0cc99b6546ed642432813bd
vicantlop <105176088+vicantlop@users.noreply.github.com>
2022-11-10 09:06:13 -0800

    migrated aticle models

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# migrations - data migrated to database - we have to make a migration file
+# commands to make migration file are
+#   - python manage.py makemigrations
+#   - python manage.py migrate
+
 class Article(models.Model):
   title = models.CharField(max_length=100)
   slug = models.SlugField()
