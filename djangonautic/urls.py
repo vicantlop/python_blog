14ab9cbc38f379bd0752a28024d5cebc9db27bb5
vicantlop <105176088+vicantlop@users.noreply.github.com>
2022-11-10 09:48:05 -0800

    created superuser and set Article models to be viewed in admin page

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
+from django.contrib import admin 
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls), #in termnal use python manage.py createsuperuser to create admin
     path('articles/', include('articles.urls')),
     path("about/", views.about),
     path('', views.home)
