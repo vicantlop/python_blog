e818d1cc12b9e91dcb301c48dfbb91c2a7438a05
vicantlop <105176088+vicantlop@users.noreply.github.com>
2022-11-11 07:24:19 -0800

    created article detail page

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.articleList)
+    path('', views.articleList, name="list"),
+    path('<slug:slug>/', views.article_detail, name='detail')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 def articleList(request):
   articles = Article.objects.all().order_by('date') #order_by allows us to order objects by any variable in the model
   return render(request, 'articles/articleList.html', {'articles': articles})
+
+def article_detail(request, slug):
+  # return HttpResponse(slug)
+  article = Article.objects.get(slug=slug)
+  return render(request, 'articles/article_detail.html', { 'article': article })
